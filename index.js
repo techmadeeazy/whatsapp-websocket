26adace89e48044d765ec0b7281f19d66f0e89d7
Daramola Femi <dfmopeoluwa@gmail.com>
2022-07-13 13:04:10 +0100

    Websocket file added to index.js

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
+import { Server } from "socket.io";
+import { createServer } from "http";
 import express from "express";
-import bodyParser from "body-parser";
+import fs from "fs";
+import path from "path";
 import axios from "axios";
+
 const app = express();
 app.use(express.json());
-let obj = "";
-app.post("/", (req, res) => {
-  obj = req.body.message.content.text;
-  res.send({ name: obj });
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
+
 app.get("/", (req, res) => {
-  res.send({ name: "obj" });
-
-  console.log(obj);
+  res.send({ hello: "hi" });
 });
 
-app.listen(4001, () => console.log("listenings on port 4001"));
+io.on("connection", (socket) => {
+  console.log("first");
+});
+
+try {
+  app.post("/", (req, res) => {
+    let message = req.body.message;
+    let messageText = req.body.message.content.text;
+
+    // if (message.direction == "received") {
+    io.emit("receivedMessage", message);
+    console.log(messageText, "res");
+    // }
+    // socket.emit("receivedMessage", message);
+
+    res.send({ have: "body" });
+  });
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  app.get("/download", (req, res) => {
+    // let message = req.body.message;
+    const url =
+      "https://firebasestorage.googleapis.com/v0/b/whatsapp-dfa36.appspot.com/o/Documentation%20Portal%20Link%20(3).pdf?alt=media&token=8fd87076-f06c-471d-9962-07a0afe40ae9";
+
+    const filePath = path.resolve(__dirname, "file", "download.png");
+    const fileStream = fs.createWriteStream(filePath);
+    res.download();
+    res.send({ hi: "femi" });
+  });
+} catch (error) {
+  console.log(error, "error");
+}
+
+httpServer.listen(5000);
